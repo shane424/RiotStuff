78452b7dd9c82c0db1df418aa7bba7496bbd27ea
shane424 <detroitdude04@gmail.com>
2015-11-20 11:57:34 -0500

    constants

---
+++ SummonerReader/RiotConsts.py
@@ -0,0 +1,20 @@
+URL = {
+    'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
+    'summoner_by_name': 'v{version}/summoner/by-name/{names}',
+    'league': 'v{version}/league/by-summoner/{summonerIds}/entry'
+}
+
+API_VERSIONS = {
+    'summoner': '1.4',
+    'matchhist': '2.2',
+    'cur_game': '1.0',
+    'static_data': '1.2',
+    'stats': '1.3',
+    'league': '2.5'
+}
+
+REGIONS = {
+    'europe_nordic_and_east': 'eune',
+    'europe_west': 'euw',
+    'north_america': 'na'
+}
